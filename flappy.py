5bb89844097030411276ec8cb887a061ead1a699
Richard Hsu <richardhsu.cs@gmail.com>
2014-03-08 11:32:44 -0800

    Animation of pipes and creation. Need to work on starting pipe.

--- flappy.py
+++ flappy.py
@@ -3,26 +3,32 @@
 import curses
 import math
 import os
+import random
 import sys
 import time
+
+PIPE_WIDTH = 8
+BIRD_WIDTH = 7
+BIRD_HEIGHT = 3
+BIRD_FRAMES = 5
 
 class EndGame(Exception):
   pass
 
 class Bird:
-  def __init__(self, nlines, ncols):
+  def __init__(self, win_nlines, win_ncols):
     # Set up the bird
-    self.x_coord = ncols / 2 - 5
-    self.y_coord = nlines / 2
-    self.bird = curses.newwin(3, 7, self.y_coord, self.x_coord)
-
+    self.x_coord = win_ncols / 2 - (BIRD_WIDTH / 2)
+    self.y_coord = win_nlines / 2
+    self.bird = curses.newwin(BIRD_HEIGHT, BIRD_WIDTH, self.y_coord, self.x_coord)
+    self.bird.scrollok(1)
     # Set up some state information and initial settings
     self.flap_up()
     self.was_flap_up = True
     self.fly_frame = 0  # Start as falling
 
     # Window Conditions
-    self.bottom = nlines
+    self.bottom = win_nlines
 
   def flap_up(self):
     self.bird.clear()
@@ -48,7 +54,7 @@ class Bird:
     self.bird.refresh()
 
   def up(self):
-    self.fly_frame = 5
+    self.fly_frame = BIRD_FRAMES
 
   def animate(self):
     if self.fly_frame > 0:
@@ -65,23 +71,30 @@ class Bird:
     self.toggle()
     self.bird.refresh()
 
-    if self.y_coord + 3 == self.bottom:
+    if self.y_coord + BIRD_HEIGHT == self.bottom:
       raise EndGame()
 
 
 class Pipe:
-  def __init__(self, delay, nlines, ncols, is_bottom, height, x, y):
+  def __init__(self, is_bottom, height, countdown, win_nlines, win_ncols):
     """
       TODO: Use window.resize(nlines, ncols) to resize the window
             Use window.mvwin(y, x) to move window
     """
-    self.x_coord = x
-    self.y_coord = y
+    self.x_orig = win_ncols - 8 - 1  # Written this way to remember win edge
+    self.x_coord = self.x_orig
+    if is_bottom:
+      self.y_coord = win_nlines - height - 1 # Again win edge
+    else:
+      self.y_coord = 1 # Top
 
-    self.pipe = curses.newwin(height, 9, self.y_coord, self.x_coord)
+    self.pipe = curses.newwin(height, PIPE_WIDTH, self.y_coord, self.x_coord)
+    self.pipe.scrollok(1)
     self.is_bottom = is_bottom
     self.height = height  # The height of the pipe
-    self.draw()
+
+    self.delay = win_ncols
+    self.countdown = countdown
 
   def draw(self):
     self.pipe.clear()
@@ -106,6 +119,57 @@ class Pipe:
 
   def refresh(self):
     self.pipe.refresh()
+
+  def animate(self):
+    if self.countdown <= 0:
+      self.x_coord -= 1
+
+      self.pipe.erase()
+      self.pipe.refresh()
+      self.pipe.mvwin(self.y_coord, self.x_coord)
+      self.draw()
+      self.pipe.refresh()
+
+      if self.x_coord == 1:
+        self.pipe.erase()
+        self.pipe.refresh()
+        self.countdown = self.delay
+    else:
+      self.countdown -= 1
+
+      if self.countdown == 0:
+        self.x_coord = self.x_orig
+
+class Pipes:
+  def __init__(self, win_nlines, win_ncols):
+    # Pipes will always be 8 in width (PIPE_WIDTH)
+    self.win_nlines = win_nlines
+    self.win_ncols = win_ncols
+
+    # Calculate the number of pipes to use
+    self.num_pipes = self.win_ncols / (int) (PIPE_WIDTH * 1.5)
+
+    self.pipes = []
+
+    init_delay = 0
+    for i in range(0, self.num_pipes):
+      top_height = random.randint(4, self.win_nlines - 10 - 4)
+      bot_height = self.win_nlines - 10 - top_height
+      self.pipes.append([Pipe(False, top_height, init_delay,
+                              self.win_nlines, self.win_ncols),
+                         Pipe(True, bot_height, init_delay,
+                              self.win_nlines, self.win_ncols)])
+      init_delay += PIPE_WIDTH * 3
+
+  def animate(self):
+    for pipe_set in self.pipes:
+      pipe_set[0].animate()
+      pipe_set[1].animate()
+
+  def refresh(self):
+    for pipe_set in self.pipes:
+      pipe_set[0].refresh()
+      pipe_set[1].refresh()
 
 class Flappy:
   def __init__(self):
@@ -142,17 +206,18 @@ class Flappy:
 
     # Init bird
     bird = Bird(self.nlines, self.ncols)
-    pipe = Pipe(0, 40, 50, True, 10, 2, 2)
-    pipe2 = Pipe(0, 40, 50, False, 10, 10, 2)
+    pipes = Pipes(self.nlines, self.ncols)
+
     while True:
       bird.refresh()
-      pipe.refresh()
-      pipe2.refresh()
+      pipes.refresh()
       key = self.window.getch()
       if key == ord('q'):
         break
       elif key == ord(' '):
         bird.up()
+
+      pipes.animate()
 
       # Refresh the loop at 0.2 seconds
       try:
